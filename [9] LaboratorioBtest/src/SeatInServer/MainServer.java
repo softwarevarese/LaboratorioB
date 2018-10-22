64d545265acdad78511e646a14d8fc7479c8fca5
ament <ament@LAPTOP-6FMM6BQ9.station>
2018-10-22 21:59:38 +0200

    New class MainServer created

---
+++ [9] LaboratorioBtest/src/SeatInServer/MainServer.java
@@ -0,0 +1,14 @@
+package SeatInServer;
+
+public class MainServer {
+	
+	private static RichiestaAlDB db = new RichiestaAlDB();
+
+	public static void main(String[] args) {
+		
+		/* TEST */
+		db.testQueryOnDB();
+
+	}
+
+}
