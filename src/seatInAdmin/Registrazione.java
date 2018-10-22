a5a5d6a2f1a9297b4ab87f02be1e10504316868e
P4o2o <P4o2o@LAPTOP-EAACQEHI>
2018-10-22 22:33:02 +0200

    commit test 22.32

--- src/seatInAdmin/Registrazione.java
+++ src/seatInAdmin/Registrazione.java
@@ -7,10 +7,10 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Registrazione extends JDialog {
-
-	private final JPanel contentPanel = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -29,27 +29,12 @@ public class Registrazione extends JDialog {
 	 * Create the dialog.
 	 */
 	public Registrazione() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		setBounds(100, 100, 450, 428);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("REGISTRAZIONE UTENTE");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setBounds(110, 13, 223, 16);
+		getContentPane().add(lblNewLabel);
 	}
-
 }
