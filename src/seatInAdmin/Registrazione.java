f2760808c6aac73851722b39fc0ba3449c3f5f6d
P4o2o <P4o2o@LAPTOP-EAACQEHI>
2018-10-22 22:50:28 +0200

    22/10 22.50

--- src/seatInAdmin/Registrazione.java
+++ src/seatInAdmin/Registrazione.java
@@ -36,5 +36,17 @@ public class Registrazione extends JDialog {
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblNewLabel.setBounds(110, 13, 223, 16);
 		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Cognome");
+		lblNewLabel_1.setBounds(44, 56, 56, 16);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nome");
+		lblNewLabel_2.setBounds(44, 79, 56, 16);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblMatricola = new JLabel("Matricola");
+		lblMatricola.setBounds(44, 108, 56, 16);
+		getContentPane().add(lblMatricola);
 	}
 }
